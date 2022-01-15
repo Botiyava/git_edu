5d714a6b3c7b8204a1daa187b8e4d9f2c4142bc1
Botiyava <botiyava.prog@mail.ru>
2022-01-15 03:37:30 +0300

    v1.02  of project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 this is simple golang programm
+Another string
+Another string number2
--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 
 func main(){
-	fmt.Println("Hello, Git!")
+	fmt.Println("Hello, Git!!!! :)")
 	}

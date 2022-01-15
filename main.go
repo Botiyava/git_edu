482bb3955f027dc1c5ade42f8a45d5ded3dc3f38
Botiyava <botiyava.prog@mail.ru>
2022-01-15 04:16:15 +0300

    added iterator

--- main.go
+++ main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 
 func main(){
-	fmt.Println("Hello, Git!!!! :)")
+	// this program contains a lot of funny stuff!!
+	fmt.Println("Hello, Git!!!! ahahah")
+	numberSlice := []int{1,2,3,4,5,6,7,8}
+	for i,_ := range numberSlice{
+		fmt.Printf("%d", i)
 	}
+}
